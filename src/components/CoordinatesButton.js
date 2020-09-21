731bc385913dcf86c3a7c7f5e85885f7e9d8f2d2
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-21 11:36:34 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class CoordinatesButton extends Component {
-mouseCoor (e){
-  let X = e.clientX;
-  let Y = e.clientY;
- this.props.onReceiveCoordinates([X,Y])
+class CoordinatesButton extends React.Component {
+  handleClick = event => {
+    this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+  };
+
+  render() {
+    return <button onClick={this.handleClick}>Coords</button>;
+  }
 }
-render() {
-    return (
-    <div >
-    <button onClick= {this.mouseCoor}>Clikc here</button>
-    </div>
-    )
-}
-}
+
+export default CoordinatesButton;
