f22ff8aca71303b9f96eea76cf6848486ef32478
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-21 11:34:56 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -4,7 +4,7 @@ export default class CoordinatesButton extends Component {
 mouseCoor (e){
   let X = e.clientX;
   let Y = e.clientY;
-return this.props.onReceiveCoordinates([X,Y])
+ this.props.onReceiveCoordinates([X,Y])
 }
 render() {
     return (
